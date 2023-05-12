eb7f6f4eb3d47ae40fe4c79eb020b075e87859ed
tornike <tornike.beridze264@gmail.com>
2023-05-12 20:42:40 +0400

    ant design app react typescript node

---
+++ React Projects/antDesign_webApp_callapp/front/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import useUsersStore from './store';
+import UserTable from './components/Table';
+import axiosInstance from './utils/axiosInstance';
+
+function App() {
+
+  const {userData,changeUserData,isChanged}  = useUsersStore()
+
+  useEffect(() => {
+    axiosInstance.get('/users').then(res => {
+      changeUserData(res.data)
+    })
+    axiosInstance.get('/chartData').then(res => {
+      console.log(res)
+    })
+  },[isChanged])
+
+  return (
+    <div className="App">
+      {userData ? (<UserTable data={userData}/>) : (<h1>Loading...</h1>)}
+    </div>
+  );
+}
+
+export default App;
